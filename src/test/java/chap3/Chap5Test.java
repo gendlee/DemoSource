55ca8fa24adc4f9697e1ef50d76ab0921e97ca04
gendlee <gendlee@sina.com>
2024-08-21 22:27:36 +0800

    布隆过滤器实现

--- src/test/java/chap3/Chap5Test.java
+++ src/test/java/chap3/Chap5Test.java
@@ -1,7 +1,10 @@
 package chap3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Chap5Test {
     public void test() {
-
+        Map<String, String> map = new HashMap(16);
     }
 }
